8b133ff61167eca21931d5f7d11357d323706974
aarya <ilanchea@wlhs.wlwv.k12.or.us>
2024-04-18 16:19:24 -0700

    Treesitter and Treesitter plagyround

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,12 +3,30 @@
 return {
   {
     'nvim-telescope/telescope.nvim', branch = '0.1.x',
+    lazy = false,
     dependencies = {'nvim-lua/plenary.nvim'} 
   },
   {
     'rebelot/kanagawa.nvim',
-    init = function()
-      vim.cmd('colorscheme kanagawa-dragon')
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("kanagawa").load('dragon')
+
+      --Sets background transparent
+      --vim.api.nvim_set_hl(0, 'Normal', {bg = 'none'})
+      --vim.api.nvim_set_hl(0, 'NormalFloat', {bg = 'none'})
     end
+  }, 
+  {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    config = function()
+      vim.cmd('TSUpdate')
+    end
+  },
+  {
+    'nvim-treesitter/playground',
+    lazy = true,
   }
 }
