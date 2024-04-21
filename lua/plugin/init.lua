f2cc556ebf0be50ef3fc3d8ea46334be522a9e4b
aarya <ilanchea@wlhs.wlwv.k12.or.us>
2024-04-21 11:55:40 -0700

    changed plugins to plugin

---
+++ lua/plugin/init.lua
@@ -0,0 +1,38 @@
+-- lazy.nvim plugin specs
+-- plugins will be loaded in this order
+return {
+  {
+    'nvim-telescope/telescope.nvim', branch = '0.1.x',
+    lazy = false,
+    dependencies = {'nvim-lua/plenary.nvim'} 
+  },
+  {
+    'rebelot/kanagawa.nvim',
+    lazy = false,
+    priority = 1000 
+  }, 
+  {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    config = function()
+      vim.cmd('TSUpdate')
+    end
+  },
+  {
+    'nvim-treesitter/playground',
+    lazy = false,
+  },
+  {
+      "ThePrimeagen/harpoon",
+      branch = "harpoon2",
+      dependencies = { "nvim-lua/plenary.nvim", 'nvim-telescope/telescope.nvim' }
+  },
+  {
+    'mbbill/undotree',
+    lazy = false,
+  },
+  {
+    'tpope/vim-fugitive',
+    lazy = false,
+  },
+}
