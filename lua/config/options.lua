928fa585f9d0329e72135910996842f4a5dd62aa
Lqnk4 <ilanchea@wlhs.wlwv.k12.or.us>
2024-04-28 16:49:59 -0700

    trouble.nvim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -36,7 +36,7 @@ vim.wo.signcolumn = "auto"
 
 -- Decrease update time
 vim.o.updatetime = 250
-vim.o.timeoutlen = 300
+vim.o.timeoutlen = 400
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = "menuone,noselect"
