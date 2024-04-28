bbd45e27de21c050d3bdcc89ddf12a6acde42b38
Lqnk4 <ilanchea@wlhs.wlwv.k12.or.us>
2024-04-28 02:13:31 -0700

    lazy loading

--- lua/plugin/fugitive.lua
+++ lua/plugin/fugitive.lua
@@ -4,6 +4,9 @@ local M = {
   keys = {
     { '<leader>gs', vim.cmd.Git },
   },
+  cmd = { "G", "Git", "GdiffSplit", "Gvdiffsplit", "Gedit", "Gsplit",
+      "Gread", "Gwrite", "Ggrep", "Glgrep", "Gmove",
+      "Gdelete", "Gremove", "Gbrowse", }
 }
 
 return M
--- lua/plugin/luasnip.lua
+++ lua/plugin/luasnip.lua
@@ -1,13 +1,10 @@
 local M = {
-	"L3MON4D3/LuaSnip",
-	-- follow latest release.
-	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-	-- install jsregexp (optional!).
-	build = "make install_jsregexp",
+  "L3MON4D3/LuaSnip",
+  -- follow latest release.
+  version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+  -- install jsregexp (optional!).
+  build = "make install_jsregexp",
   dependencies = { "rafamadriz/friendly-snippets" },
-  init = function()
-    require('luasnip.loaders.from_vscode').lazy_load()
-  end,
   event = {
     "InsertEnter"
   }
